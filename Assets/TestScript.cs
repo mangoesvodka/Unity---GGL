774d716dd1806ed67910eeb198dbc20f14655be9
DreamMachine <sandyde123@gmail.com>
2024-03-13 00:47:13 +0530

    -Added an empt game object called Test -Added a Test script with a comment within

---
+++ Assets/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //THIS IS A COMMENT ABOUT HOW I AM A NOOB IRL
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
